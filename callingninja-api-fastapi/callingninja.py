0af86666d743af125a973dca0ff852f48f3dce0b
derwaro <chrdina@gmail.com>
2023-06-21 23:01:16 -0600

    works auth!

--- callingninja-api-fastapi/callingninja.py
+++ callingninja-api-fastapi/callingninja.py
@@ -1,4 +1,5 @@
 import asyncio
+import base64
 from typing import Union, Annotated
 from fastapi import (
     FastAPI,
@@ -14,10 +15,12 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import motor.motor_asyncio
 from pydantic import BaseModel, HttpUrl
 from starlette.middleware.sessions import SessionMiddleware
+from src.security import JWTBearer
 
 import csv
 import os
 import uuid
+import requests
 
 
 from dotenv import load_dotenv, find_dotenv
@@ -37,7 +40,8 @@ session_secret_key = (
 )
 app.add_middleware(SessionMiddleware, secret_key=session_secret_key)
 # init oauth2
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# not needed bc custom jwt auth in security.py
 
 # get enviroment variable file
 load_dotenv(find_dotenv())
@@ -59,9 +63,9 @@ client = Client(account_sid, auth_token)
 s3 = boto3.client("s3")
 
 # connect to mongodb
-db_client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+# db_client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 # select collection from mongodb
-db = db_client.tpv
+# db = db_client.tpv
 
 
 # pydantic Model
@@ -76,25 +80,6 @@ class UserInDB(User):
     hashed_password: str
 
 
-# fake user table
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
-
-
 ###############################################################################
 ###############################################################################
 # endpoints
@@ -102,34 +87,35 @@ fake_users_db = {
 ###############################################################################
 
 
-# AUTH TESTS
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+# works
+# used to manually obtain a token from api-user, copy from response to JWTBearer login in fastapi swagger ui
+@app.post("/custom_token")
+async def custom_token(username: str, password: str):
+    url = "http://localhost:8081/users/token"
+    auth_base64_base = username + ":" + password
+    auth_base64_encoded = base64.b64encode(auth_base64_base.encode("utf-8")).decode(
+        "utf-8"
     )
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
-    return current_user
-
-
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
+    headers = {
+        "accept": "*/*",
+        "Authorization": f"Basic {auth_base64_encoded}",
+        "Content-Type": "application/json",
+    }
+    payload = {
+        "username": username,
+        "password": password,
+        "authorities": [{"authority": "string"}],
+        "accountNonExpired": True,
+        "accountNonLocked": True,
+        "credentialsNonExpired": True,
+        "enabled": True,
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    if response.status_code == 200:
+        response = response.json()
+        return {"token": response["token"]}
+    else:
+        return {"token": None, "message": "Invalid user or password"}
 
 
 ##################################################################################################
@@ -173,7 +159,9 @@ async def upload_audio(uploaded_audio: UploadFile):
     summary="Async Upload to S3",
     description="Upload a file to a S3 bucket using the async wrapper for boto3 - aiboto3 - async",
 )
-async def upload_audio_async(uploaded_audio: UploadFile, request: Request):
+async def upload_audio_async(
+    uploaded_audio: UploadFile, request: Request, admin=Depends(JWTBearer(["ADMIN"]))
+):
     try:
         if magic.from_file(uploaded_audio.filename, mime=True).split("/")[0] == "audio":
             session = asyncboto.Session()
@@ -277,6 +265,35 @@ async def call(request: Request):
         }
 
 
+@app.post("/call_manual")
+async def call_manual(
+    from_number: str,
+    to_number: str,
+    audio_url: str,
+    customer=Depends(JWTBearer(["CUSTOMER"])),
+):
+    # init twilio client
+    client = Client(customer.twilio_sid, customer.twilio_token)
+    # set url for callbacks on call events
+    status_callback_url = "https://534c-189-216-168-189.ngrok-free.app/call_status"
+    # send call request to twilio api
+    client.calls.create(
+        method="GET",
+        status_callback=status_callback_url,
+        status_callback_event=["initiated", "ringing", "answered", "completed"],
+        status_callback_method="POST",
+        url=audio_url,
+        to=to_number,
+        from_=from_number,
+    )
+    return {
+        "message": "Call initiated successfully",
+        "Recipients; to": to_number,
+        "Emitter; from": from_number,
+        "Played Audio Message; url": audio_url,
+    }
+
+
 @app.post("/call_status")
 async def call_status(call_status):
     fieldnames = call_status.keys()
@@ -288,7 +305,21 @@ async def call_status(call_status):
     return {"call_status": call_status}
 
 
-@app.get("/example")
-async def example(request: Request, token: Annotated[str, Depends(oauth2_scheme)]):
+@app.get("/example_customer")
+async def example_customer(request: Request, customer=Depends(JWTBearer(["CUSTOMER"]))):
     host = request.client.host
-    return {"host": host, "token": token}
+    return {"host": host, "current_user": customer}
+
+
+@app.get("/example_admin")
+async def example_admin(request: Request, admin=Depends(JWTBearer(["ADMIN"]))):
+    host = request.client.host
+    return {"host": host, "current_user": admin}
+
+
+@app.get("/example_all")
+async def example_all(
+    request: Request, user=Depends(JWTBearer(["ADMIN", "CUSTOMER", "OPERATOR"]))
+):
+    host = request.client.host
+    return {"host": host, "current_user": user}
