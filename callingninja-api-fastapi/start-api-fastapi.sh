06291177b3cba43fedc350986630bb0382820658
derwaro <derwaro@proton.me>
2023-07-18 10:51:05 -0600

    change build commands

--- callingninja-api-fastapi/start-api-fastapi.sh
+++ callingninja-api-fastapi/start-api-fastapi.sh
@@ -1,4 +1,4 @@
 ## <!-- Designed by Neftali Ramirez Chavez Julio 2023 nefkon@gmail.com -->
 cd /usr/src/app/deployment/callingninja-api-fastapi/
 sleep 20
-python -m uvicorn callingninja:app --host 127.0.0.1 --port 80
+python -m uvicorn callingninja:app --host 127.0.0.1 --port 8000
